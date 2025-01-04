a2f084470e4bf69a734eea9e73d4ee3b3407fffd
christymanthara <metalliccj@gmail.com>
2025-01-04 16:28:31 +0100

    added logging to subscriber

--- gen6s/gen6s_bnw/src/gen6s_bnw_node.cpp
+++ gen6s/gen6s_bnw/src/gen6s_bnw_node.cpp
@@ -8,6 +8,8 @@ class Gen6sBnwNode : public rclcpp::Node
 public:
     Gen6sBnwNode() : Node("gen6s_bnw_node")
     {
+        RCLCPP_INFO(this->get_logger(), "Waiting for a color image on topic '/imu/data'...");
+
         subscription_ = this->create_subscription<sensor_msgs::msg::Image>(
             "/imu/data", 10,
             std::bind(&Gen6sBnwNode::imageCallback, this, std::placeholders::_1));
@@ -18,6 +20,10 @@ private:
     {
         try
         {
+            // Log that an image has been received
+            RCLCPP_INFO(this->get_logger(), "Obtained a color image from /imu/data");
+
+            // Convert ROS image to OpenCV image
             auto cv_ptr = cv_bridge::toCvCopy(msg, "bgr8");
 
             // Convert to grayscale
@@ -28,7 +34,7 @@ private:
             cv::Mat binary_image;
             cv::threshold(gray_image, binary_image, 128, 255, cv::THRESH_BINARY);
 
-            // Display the image
+            // Display the binary image
             cv::imshow("Black and White Image", binary_image);
             cv::waitKey(1);
         }
