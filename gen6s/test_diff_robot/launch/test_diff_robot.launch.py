c2fde2c6bed251afff0b9c4c99a2b9352802cf31
christymanthara <metalliccj@gmail.com>
2025-01-22 00:29:09 +0100

    Fix:minor fixes to TF

--- gen6s/test_diff_robot/launch/test_diff_robot.launch.py
+++ gen6s/test_diff_robot/launch/test_diff_robot.launch.py
@@ -86,12 +86,20 @@ def generate_launch_description():
             arguments=['--ros-args', '--log-level', 'info'] #added logging to inspect errors
         ),
 
+        # Joint State Publisher Node
         Node(
-            package='joint_state_publisher_gui',
-            executable='joint_state_publisher_gui',
-            name='joint_state_publisher_gui',
+            package='joint_state_publisher',
+            executable='joint_state_publisher',
+            name='joint_state_publisher',
             output='screen'
         ),
+
+        # Node(
+        #     package='joint_state_publisher_gui',
+        #     executable='joint_state_publisher_gui',
+        #     name='joint_state_publisher_gui',
+        #     output='screen'
+        # ),
 
         Node(
                 package='controller_manager',
